6bf4ac5e5ebad94aa69814e4c117af428e8dbbc5
ramses1998 <ramsestetang2017@outlook.com>
2024-06-09 03:03:30 +0200

    Umgebungvariable als ENV statt ARG in Dockerfile

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -36,7 +36,7 @@ const authOptions: AuthOptions = {
         }),
     ],
 
-    secret: "IbcJYuGwT7V/BGyhijc/USevOJdArymV4umChLb4Kt4=",
+    secret: process.env.NEXTAUTH_SECRET ?? "IbcJYuGwT7V/BGyhijc/USevOJdArymV4umChLb4Kt4=",
 
     callbacks: {
         // @ts-ignore
