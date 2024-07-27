0870189155cb18023ce472a4793b7dfab0fb2686
ramses <ramsestetang2017@outlook.com>
2024-07-27 15:09:08 +0200

    API-Versionierung für Backend-Aufrufe

--- api/httpClient.ts
+++ api/httpClient.ts
@@ -1,7 +1,7 @@
 import axios, {AxiosInstance} from "axios";
 
 export const axiosClient: AxiosInstance = axios.create({
-    baseURL: "http://localhost:8081",
+    baseURL: "http://localhost:8081/api/v1",
     headers: {
         "Content-Type": "application/json",
     },
