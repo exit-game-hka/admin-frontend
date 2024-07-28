3e04765848c092b473beb32919bdb0d2a10d2787
ramses <ramsestetang2017@outlook.com>
2024-07-28 16:16:30 +0200

    NEXT_PUBLIC_BACKEND_URL als BASE_URL des http clients

--- api/httpClient.ts
+++ api/httpClient.ts
@@ -1,7 +1,7 @@
 import axios, {AxiosInstance} from "axios";
 
 export const axiosClient: AxiosInstance = axios.create({
-    baseURL: "http://localhost:8081/api/v1",
+    baseURL: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1`,
     headers: {
         "Content-Type": "application/json",
     },
